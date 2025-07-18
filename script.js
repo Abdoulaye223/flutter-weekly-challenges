92c3f2cac5ff0fa478d07359bd4592f662f2ec5e
Abdoulaye223 <156548566+Abdoulaye223@users.noreply.github.com>
2025-07-18 11:05:38 +0000

    Add files via upload

--- script.js
+++
@@ -1,5 +0,0 @@
-// Simple form handling
-        document.querySelector('.email-form').addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Thanks for subscribing! (This is a demo - set up your email service)');
-        });
